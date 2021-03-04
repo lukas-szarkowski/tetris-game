714ffb784b1b14857ac09751ad42426bf62a6623
Łukasz Szarkowski <lukas.szarkowski@gmail.com>
2021-03-04 20:08:50 +0100

    basic tetris core, control buttons, integrating bootstrap, simple styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {TetrisCoreModule} from 'ngx-tetris';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TetrisCoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
